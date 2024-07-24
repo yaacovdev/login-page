0a5d7de3f611024a06245919673b9fe38d406256
yaacovdev <yaacov@purpl-ride.com>
2024-07-24 15:10:59 +0300

    chore: Update LoginPage layout for responsive design

--- frontend/web-registration/src/screens/LoginPage.tsx
+++ frontend/web-registration/src/screens/LoginPage.tsx
@@ -4,14 +4,16 @@ import LeftSide from "../components/LeftSide";
 import RightSide from "../components/RightSide";
 
 const LoginPage: React.FC = () => {
-    
     return (
         <div
-            className="flex flex-col md:flex-row bg-white rounded-2xl shadow-2xl overflow-hidden max-w-6xl w-full h-screen mx-auto mt-10"
-            style={{ maxHeight: "75.6667vh" }}
+            className="flex flex-col lg:flex-row bg-white rounded-2xl shadow-2xl overflow-hidden lg:max-w-[64%]  mx-auto"
         >
-            <LeftSide />
-            <RightSide />
+            <div className="flex w-full lg:w-[58.8%] justify-center items-center bg-primary">
+                <LeftSide />
+            </div>
+            <div className="flex w-full lg:w-[41.2%] justify-center items-center">
+                <RightSide />
+            </div>
         </div>
     );
 };
